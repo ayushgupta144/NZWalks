431095039f2c9ff27c524ebd3033727a40cbc66b
ayushgupta144 <ayushgupta1447@gmail.com>
2025-04-28 08:58:17 +0530

    comments

--- NZWalks.API/Models/DTO/DifficultyDto.cs
+++ NZWalks.API/Models/DTO/DifficultyDto.cs
@@ -1,9 +1,18 @@
 ﻿namespace NZWalks.API.Models.DTO
 {
+    /// <summary>
+    /// Difficu;ty
+    /// </summary>
     public class DifficultyDto
     {
+        /// <summary>
+        /// Id
+        /// </summary>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Name
+        /// </summary>
         public string Name { get; set; }
     }
 }
