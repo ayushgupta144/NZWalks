92109a9ede9c03420ea772a834b58987bcb25bcd
ayushgupta144 <ayushgupta1447@gmail.com>
2025-05-26 20:09:27 +0530

    v2

--- NZWalks.API/Models/DTO/AddRegionRequestDto.cs
+++ NZWalks.API/Models/DTO/AddRegionRequestDto.cs
@@ -5,10 +5,19 @@
     /// </summary>
     public class AddRegionRequestDto
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public string Code { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public string? RegionImageUrl { get; set; }
     }
 }
